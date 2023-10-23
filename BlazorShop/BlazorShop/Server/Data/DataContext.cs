88674e6cafea6160c7817b6077163c4c3dec48ce
Phillip Lu <Plu2978@conestogac.on.ca>
2023-10-23 10:14:12 -0400

    created orders tables

--- BlazorShop/BlazorShop/Server/Data/DataContext.cs
+++ BlazorShop/BlazorShop/Server/Data/DataContext.cs
@@ -18,6 +18,9 @@ namespace BlazorShop.Server.Data
             modelBuilder.Entity<ProductVariant>()
                 .HasKey(p => new { p.ProductId, p.ProductTypeId });
 
+            modelBuilder.Entity<OrderItem>()
+                .HasKey(oi => new { oi.OrderId, oi.ProductId, oi.ProductTypeId });
+
             modelBuilder.Entity<ProductType>().HasData(
                     new ProductType { Id = 1, Name = "Default" },
                     new ProductType { Id = 2, Name = "PS5" },
@@ -282,5 +285,7 @@ namespace BlazorShop.Server.Data
         public DbSet<ProductVariant> ProductVariants { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
     }
 }
